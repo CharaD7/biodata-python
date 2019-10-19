a89daf54bfed0dd138b09d7640dd26eb621265e6
Joy Ayitey <jijakahn6@gmail.com>
2019-10-19 17:49:38 +0100

    Registered our model

--- biodata/admin.py
+++ biodata/admin.py
@@ -1,3 +1,13 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
+from .models import Employee
+
 
 # Register your models here.
+@admin.register(Employee)
+
+class EmployeeAdmin(ImportExportModelAdmin):
+    """
+        This is to allow admin upload excel files of biodata
+    """
+    pass
